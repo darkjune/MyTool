ebde9d1d22ad9a8954ae7119af675076022ed5a4
darkjune <darkjune@gmail.com>
2014-03-11 23:34:11 +0800

    init

---
+++ src/com/tool/Immutable.java
@@ -0,0 +1,11 @@
+package com.tool;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 14-3-1
+ * Time: 下午3:52
+ * To change this template use File | Settings | File Templates.
+ */
+public @interface Immutable {
+}
