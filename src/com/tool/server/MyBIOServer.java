a2a22bb5f100c3aca52fa4753de6998265b7d583
darkjune <ryan_zhu@PRCHAZ11870D.ad.infosys.com>
2014-05-20 17:58:29 +0800

    echo change.

--- src/com/tool/server/MyBIOServer.java
+++ src/com/tool/server/MyBIOServer.java
@@ -18,6 +18,16 @@ import java.util.logging.Logger;
  * @author ryan_zhu
  */
 public class MyBIOServer {
+    public static final String SAMPLE_HTML_RESPONSE = "HTTP/1.1 200 OK\r\n"+
+                                                      "Cache-Control:max-age=0\r\n"+
+                             "Connection:Keep-Alive\r\n"
+                             +"Content-Type:text/html; charset=utf-8\r\n"
+                             +"Date:Tue, 18 Feb 2014 05:22:40 GMT\r\n"
+                             +"Keep-Alive:timeout=5, max=99\r\n"
+                             +"Server:Apache/2.2.16 (Win32) mod_auth_sspi/1.0.4 mod_perl/2.0.4 Perl/v5.10.0\r\n"
+                             +"Set-Cookie:FOSWIKISID=f5c43ac7a23d4addd754f7e3c1fccd44; path=/; HttpOnly\r\n"
+                                                      +"\r\n"
+                                                      +"<html>test</html>\r\n";
     public static void main(String[]args){
         
         try {
@@ -32,16 +42,7 @@ public class MyBIOServer {
                  };
                  
                  String response = 
-                         "HTTP/1.1 200 OK\r\n"+
-                         "Cache-Control:max-age=0\r\n"+
-"Connection:Keep-Alive\r\n"
-+"Content-Type:text/html; charset=utf-8\r\n"
-+"Date:Tue, 18 Feb 2014 05:22:40 GMT\r\n"
-+"Keep-Alive:timeout=5, max=99\r\n"
-+"Server:Apache/2.2.16 (Win32) mod_auth_sspi/1.0.4 mod_perl/2.0.4 Perl/v5.10.0\r\n"
-+"Set-Cookie:FOSWIKISID=f5c43ac7a23d4addd754f7e3c1fccd44; path=/; HttpOnly\r\n"
-                         +"\r\n"
-                         +"<html>test</html>\r\n"
+                         SAMPLE_HTML_RESPONSE
                          ;
                  s.getOutputStream().write(response.getBytes());
                  s.close();
--- src/com/tool/server/NIOSimpleEchoServer.java
+++ src/com/tool/server/NIOSimpleEchoServer.java
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This class is to demonstrate single thread SUN NIO channel server using, 
+ * which as known a Reactor in single thread, only main thread will handle coming connection.
  */
 package com.tool.server;
 
@@ -13,7 +13,6 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -43,13 +42,15 @@ public class NIOSimpleEchoServer implements Runnable {
             selector = Selector.open();
             ssc.socket().bind(new InetSocketAddress(8080));
             ssc.configureBlocking(false);
+
+            //register selector on this channel with instrest OP.
             ssc.register(selector, SelectionKey.OP_ACCEPT);
 
             while (true) {
 
                 int n = selector.select(); //block when no event.
                 if (n > 0) {
-                    System.out.println("selector hit:" + count.incrementAndGet());
+                    System.out.println("\nSelector running round:" + count.incrementAndGet());
                     Set<SelectionKey> keys = selector.selectedKeys();
 
                     Iterator<SelectionKey> iterator = keys.iterator();
@@ -57,32 +58,53 @@ public class NIOSimpleEchoServer implements Runnable {
                         SelectionKey key = iterator.next();
                         iterator.remove();
                         if (key.isAcceptable()) {
-                            System.out.println("Handle key.isAcceptable:" + key);
+                            System.out.println("Handle SelectionKey.OP_ACCEPT:Key->" + key + ";\nKey.Channel->" + key.channel());
 
-
+                            //handle server channel accept event.
                             ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
                             SocketChannel socketChannel = serverChannel.accept();
+
+                            System.out.println("new socket channel->" + socketChannel.toString()
+                                    + " generate. register current selector on this channel with OP_READ.");
+                            //handle remote client channel, register this selector on this channel.
                             socketChannel.configureBlocking(false);
                             socketChannel.register(selector, SelectionKey.OP_READ);
                         } else if (key.isReadable()) {
-                            System.out.println("Handle key.isReadable:" + key);
-                            SocketChannel serverChannel = (SocketChannel) key.channel();
+                            System.out.println("Handle SelectionKey.OP_READ:Key->" + key + ";\nKey.Channel->" + key.channel());
+                            SocketChannel socketChannel = (SocketChannel) key.channel();
                             buffer.clear();
-                            int i = serverChannel.read(buffer);
+                            int i = socketChannel.read(buffer);
                             if (i > 0) {
                                 buffer.flip();
                                 byte[] buf = Arrays.copyOfRange(buffer.array(), 0, i);
-                                messages.put(serverChannel, buf);
-                                key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
-                                System.out.println("read from:" + serverChannel.socket().getRemoteSocketAddress()
-                                        + "\nmessage:\n" + new String(buf));
+                                messages.put(socketChannel, buf);
+                                key.attach(new String("1"));
+                                System.out.println("read from:" + socketChannel.socket().getRemoteSocketAddress()
+                                        + "\nMESSAGE:\n" + new String(buf) + "\nKey.attach->" + key.attachment());
+//                                socketChannel.register(selector, SelectionKey.OP_WRITE);
+                                socketChannel.register(selector, SelectionKey.OP_WRITE, new String("1"));
+                                /**
+                                 * 这里用interestOps取代register(Selector sel, int
+                                 * ops)是因为用register(Selector sel, int
+                                 * ops会导致selector中保存的SelectionKey
+                                 * 被更新，原SelectionKey的attachment丢失。
+                                 * 也可以用register(Selector sel, int ops,Object
+                                 * att)来附上attachment，与使用interestOps等价。
+                                 * 写通道90%时间是一直开放的，一般不需要注册，这里仅作示例。
+                                *
+                                 */
+//                                key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
+                                System.out.println("\nAfter register, Key.attach->" + key.attachment());
+
+
 
                             } else if (i == -1) {
                                 key.cancel();
                             }
 
                         } else if (key.isWritable()) {
-                            System.out.println("Handle key.isWritable:" + key);
+                            System.out.println("Handle SelectionKey.OP_WRITE:Key->" + key + ";\nKey.Channel->" + key.channel()
+                                    + "\nKey.attach->" + key.attachment());
                             SocketChannel channel = (SocketChannel) key.channel();
                             byte[] buf = messages.get(channel);
                             if (buf != null) {
@@ -90,10 +112,12 @@ public class NIOSimpleEchoServer implements Runnable {
                                 key.interestOps(SelectionKey.OP_READ);
                                 buffer.clear();
                                 buffer.put(buf);
+//                                buffer.put(MyBIOServer.SAMPLE_HTML_RESPONSE.getBytes());
                                 buffer.flip();
                                 channel.write(buffer);
                                 System.out.println("Write to:" + channel.socket().getRemoteSocketAddress()
-                                        + " \nmessage:\n" + new String(buf));
+                                        + " \nMESSAGE:\n" + new String(buf));
+                                channel.close();
                             }
                         }
                     }
