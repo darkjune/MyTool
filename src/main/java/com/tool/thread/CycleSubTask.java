e6cacd6ab11f161aca346ecf1b882e411222828b
ryan zhu <darkjune@gmail.com>
2014-06-17 21:22:27 +0800

    Add CyclicBarrier

--- src/main/java/com/tool/thread/CycleSubTask.java
+++ src/main/java/com/tool/thread/CycleSubTask.java
@@ -1,18 +1,39 @@
 package com.tool.thread;
 
 import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 /**
+ * Random generate number, then wait other thread reach barrier.
  * Created by kunrong on 14-6-15.
  */
 public class CycleSubTask implements Runnable{
+    CyclicBarrier cb ;
+    long[] arr;
+    int id;
+
+
+
+    public CycleSubTask(CyclicBarrier cb, long[] arr, int id){
+        this.cb = cb;
+        this.arr = arr;
+        this.id = id;
+    }
+
     public void run() {
         long sleepTime = Math.round(Math.random()*8999+1000);
-        System.out.println("Thread going to sleep " + sleepTime + " millseconds.");
+        arr[id] = sleepTime;
+
+        System.out.println("Thread-" +id +": number = " + sleepTime + ".");
         try {
-            Thread.sleep(sleepTime);
+            cb.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
         }
+        System.out.println("Thread-" +id +" awake.");
+
     }
 }
--- src/main/java/com/tool/thread/MainCycle.java
+++ src/main/java/com/tool/thread/MainCycle.java
@@ -1,14 +1,39 @@
 package com.tool.thread;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
+ * Make a barrier, then start child thread generate number, after all child get number,
+ * plus all.
  * Created by kunrong on 14-6-15.
  */
 public class MainCycle {
     public static void main(String[] args){
-        CyclicBarrier cb = new CyclicBarrier(3, new CycleSubTask());
+        final long[] arr = new long[5];
+        CyclicBarrier cb = new CyclicBarrier(3, new Runnable(){
+            public void run() {
+                System.out.println("All task at barrier,start custom thread.");
+                long total = 0;
+                for(int i=0;i<arr.length;i++){
+                    total = total + arr[i];
+                }
+                System.out.println("Total:" + total);
+            }
+        });
+
+        for(int i =1;i<4;i++){
+            Thread t = new Thread(new CycleSubTask(cb,arr,i));
+            t.start();
+        }
+
+        System.out.println("In main thread again.");
+
+
 
     }
+
+
 
 }
