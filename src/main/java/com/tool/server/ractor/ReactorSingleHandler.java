923ba3c5381f8181ef7530198ccc61c7cda2daea
darkjune <ryan_zhu@PRCHAZ11870D.ad.infosys.com>
2014-06-25 09:34:46 +0800

    sample code in reactor server

--- src/main/java/com/tool/server/ractor/ReactorSingleHandler.java
+++ src/main/java/com/tool/server/ractor/ReactorSingleHandler.java
@@ -5,27 +5,34 @@
 package com.tool.server.ractor;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author ryan_zhu
  */
-class ReactorSingleHandler implements Runnable{
-    
+class ReactorSingleHandler implements Runnable {
+
     SocketChannel sc;
     SelectionKey sk;
     Selector selector;
-    
+    ByteBuffer in = ByteBuffer.allocate(1024);
+    ByteBuffer out = ByteBuffer.allocate(1024);
+    static final int READING = 0, SENDING = 1;
+    int state = READING;
+
     public ReactorSingleHandler(Selector selector, SocketChannel sc) {
         this.sc = sc;
         try {
             sc.configureBlocking(false);
             sk = sc.register(selector, 0); //?
             sk.attach(this);
-            System.out.println("Handler..");
+            System.out.println("Handler start..");
             sk.interestOps(SelectionKey.OP_READ);
             selector.wakeup();
         } catch (IOException e) {
@@ -36,7 +43,42 @@ class ReactorSingleHandler implements Runnable{
 
     @Override
     public void run() {
+        try {
+            if (state == READING) {
+                read();
+            } else if (state == SENDING) {
+                write();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ReactorSingleHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
+    void read() throws IOException {
+        
+        sc.read(in);
+        System.out.println("read: " + in.toString());
+        if(isInputComplete()){
+            state = SENDING;
+            sk.interestOps(SelectionKey.OP_WRITE);
+            
+        }
+        in.clear();
+    }
+
+    void write() throws IOException {
+        sc.write(out);
+        if(isOutputComplete()){
+            sk.cancel();
+        }
     }
     
+    boolean isInputComplete() throws IOException{
+        if (sc.read(in)>0) return false;
+        else return true;
+    }
+    
+    boolean isOutputComplete(){
+        return false;
+    }
 }
